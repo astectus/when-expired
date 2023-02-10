258935a2675a0886648be487a641677a35a8877e
Dumitru Dascal <dascal.dima@gmail.com>
2023-02-10 15:48:57 +0100

    fix(add-product): added context

--- utils/typeChecker.ts
+++ utils/typeChecker.ts
@@ -3,6 +3,7 @@ import Product from '../models/Product';
 
 export function isProduct(object: unknown): object is Product {
   if (object !== null && typeof object === 'object') {
+    // @ts-ignore
     return 'name' in object && 'expirationDate' in object;
   }
 
@@ -11,6 +12,7 @@ export function isProduct(object: unknown): object is Product {
 
 export function isImage(object: unknown): object is Image {
   if (object !== null && typeof object === 'object') {
+    // @ts-ignore
     return 'uri' in object;
   }
 
