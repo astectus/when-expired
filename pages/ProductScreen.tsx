f79482d2dfa138283f35228e724a71b3134c5602
dasca <dascal.dima@gmail.com>
2024-06-03 23:22:21 +0200

    feat: add product details page and description

--- pages/ProductScreen.tsx
+++ pages/ProductScreen.tsx
@@ -1,19 +1,67 @@
-import { Text } from 'react-native-paper';
+import { Text, Image, StyleSheet, ScrollView } from 'react-native';
 import { View } from 'react-native';
 import { NavigationProp } from '@react-navigation/native';
 import Product from '../models/Product';
+import { Category } from '../models/Category';
 
 export default function ProductScreen({
-  route,
-}: {
+                                        route,
+                                      }: {
   navigation: NavigationProp<any>;
   route: any;
 }) {
   const { product } = route.params as { product: Product };
 
   return (
-    <View>
-      <Text>{product.name}</Text>
-    </View>
+    <ScrollView>
+      <View style={styles.container}>
+        {product.photoUri && <Image source={{ uri: product.photoUri }} style={styles.image} />}
+        <Text style={styles.title}>{product.name}</Text>
+        <Text style={styles.description}>{product.description}</Text>
+        <Text style={styles.price}>{product.price}</Text>
+        <View style={styles.categoriesContainer}>
+
+        </View>
+      </View>
+    </ScrollView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 16,
+  },
+  image: {
+    width: 200,
+    height: 200,
+    borderRadius: 100,
+    alignSelf: 'center',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    marginTop: 16,
+  },
+  description: {
+    fontSize: 16,
+    textAlign: 'center',
+    marginTop: 8,
+  },
+  price: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    marginTop: 8,
+  },
+  categoriesContainer: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'center',
+    marginTop: 8,
+  },
+  category: {
+    fontSize: 14,
+    margin: 4,
+  },
+});
